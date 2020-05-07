8648f2961059cdfd5725e218d1416714fa799004
Sandra Choy <s10175863@connect.np.edu.sg>
2020-05-07 20:37:15 +0800

    practical 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,12 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView babyImage = findViewById(R.id.babyImage);
+        babyImage.setImageResource(R.drawable.cute_baby);
+
+        MediaPlayer audioBaby = MediaPlayer.create(this, R.raw.baby_laughing);
+        audioBaby.start();
+
+        VideoView rabbidVideo = findViewById(R.id.rabbidVideo);
+        rabbidVideo.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid)
+        );
+        rabbidVideo.start();
     }
 }
